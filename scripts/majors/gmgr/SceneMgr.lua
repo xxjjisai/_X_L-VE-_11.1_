bd5c964582aee80b2a26f96f6a3884912e3686d7
xxjjisai <m7jason@hotmail.com>
2019-01-26 11:03:37 +0800

    动画播放完毕回调测试

--- scripts/majors/gmgr/SceneMgr.lua
+++ scripts/majors/gmgr/SceneMgr.lua
@@ -187,6 +187,28 @@ function SceneMgr:KeyBoardDown(key, scancode, isrepeat)
             self.nPlayerIndex = self.nPlayerIndex + 1;
             self.iPlayer,self.nPlayerIndex = self.tbCurScene:GetPlayer(self.nPlayerIndex)
         end 
+
+        -- 切换动画
+        if key == "1" then 
+            local iAnimateSys = self.tbCurScene:GetSystemByName("AnimationSystem");
+            self.iPlayer:ChangeiCompoParam({
+                ["Animate"] = { sImg = "ball", nQuadW = 32, nQuadH = 32, nTotalFrame= 5, nLoop = 1, nTotalPlayCount = 10,nTimeAfterPlay = 0.07 };
+            })
+            iAnimateSys:ReSetFrame(self.iPlayer);
+            iAnimateSys:Play(self.iPlayer);
+        end 
+        if key == "2" then 
+            local iAnimateSys = self.tbCurScene:GetSystemByName("AnimationSystem");
+            self.iPlayer:ChangeiCompoParam({
+                ["Animate"] = { sImg = "tc", nQuadW = 30, nQuadH = 55, nTotalFrame= 18, nLoop = 0, nTotalPlayCount = 5,nTimeAfterPlay = 0.07 };
+            })
+            iAnimateSys:ReSetFrame(self.iPlayer);
+            iAnimateSys:Play(self.iPlayer,function()
+                ActorMgr:RemoveActor(self.iPlayer)
+            end);
+        end
+
+
     end
 end
 
